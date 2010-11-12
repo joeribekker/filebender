d18e639c8e65c5d34c087c3c8bd4cbdc7fec1264
gijsmolenaar <gijsmolenaar@81ab10f9-9c10-7b2c-b787-c0fe54bfbca0>
2010-11-12 19:44:41 +0000

    working on layout

--- trunk/filebender/src/filebender/files/forms.py
+++ trunk/filebender/src/filebender/files/forms.py
@@ -3,5 +3,7 @@ from django import forms
 
 class UploadForm(forms.Form):
     file = forms.FileField()
+    message = forms.CharField(widget=forms.Textarea())
     receiver = forms.EmailField()
+
 
--- trunk/filebender/src/filebender/urls.py
+++ trunk/filebender/src/filebender/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,3 +10,8 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
