c163ef4e71969cdfbf78f4e4bd8c78866bfcb6d8
gijsmolenaar <gijsmolenaar@81ab10f9-9c10-7b2c-b787-c0fe54bfbca0>
2010-11-17 13:10:00 +0000

    upload is now working, trying to get saml2 to work

--- trunk/filebender/src/filebender/urls.py
+++ trunk/filebender/src/filebender/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^files/', include('filebender.files.urls')),
-#    (r'^saml2/', include('djangosaml2.urls')),
+    (r'^saml2/', include('djangosaml2.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
