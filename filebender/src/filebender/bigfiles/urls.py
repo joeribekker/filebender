d8ad8839a1490027fb8f82c922e1b09c5df378fb
gijsmolenaar <gijsmolenaar@81ab10f9-9c10-7b2c-b787-c0fe54bfbca0>
2010-11-18 11:02:37 +0000

    renamed Files to BigFiles to prevent name collision

--- filebender/src/filebender/bigfiles/urls.py
+++ filebender/src/filebender/bigfiles/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns
 from django.conf import settings
 
 urlpatterns = patterns('',
     (r'^list/$', 'files.views.list'),
-    (r'^download/(?P<id>\d+)/(?P<secret>\w{50})/$', 'files.views.download'),
+    (r'^download/(?P<id>\d+)/(?P<secret>\w{%i})/$' % settings.FILE_SECRET_LENGTH,
+        'files.views.download'),
     (r'^delete/(?P<id>\d+)/$', 'files.views.delete'),
     (r'^upload/$', 'files.views.upload'),
     (r'^$', 'files.views.list'),
