97cc3fd7813a4e3478ef1a6444e11d1d78bcc819
gijsmolenaar <gijsmolenaar@81ab10f9-9c10-7b2c-b787-c0fe54bfbca0>
2010-11-19 14:54:28 +0000

    javascript upload >4gb working with safari4 and google chrome

--- filebender/src/filebender/bigfiles/forms.py
+++ filebender/src/filebender/bigfiles/forms.py
@@ -1,14 +1,13 @@
 
 from django import forms
-import datetime
+from bigfiles.models import one_week_later
 
 class UploadForm(forms.Form):
     file = forms.FileField('File')
     message = forms.CharField(widget=forms.Textarea(),label='Message',
                               required=False)
-    receiver = forms.EmailField(label='Receiver')
-    
-    default_expire = datetime.datetime.now() + datetime.timedelta(weeks=1)
-    expire_date = forms.DateTimeField(label='Expire date', initial=default_expire)
+    receiver = forms.EmailField(label='Receiver', required=False)
+    expire_date = forms.DateTimeField(label='Expire date',
+                                      initial=one_week_later, required=False)
 
 
