783a6c6d90f1479e36743bf59d86dbb868d02341
gijsmolenaar@gmail.com <gijsmolenaar@gmail.com@81ab10f9-9c10-7b2c-b787-c0fe54bfbca0>
2011-10-04 16:42:06 +0000

    working on blob crypter

--- filebender/media/js/upload.js
+++ filebender/media/js/upload.js
@@ -1,51 +1,90 @@
-    function fileSelected() {
-        var file = document.getElementById('file').files[0];
-        if (file) {
-          var fileSize = 0;
-          if (file.size > 1024 * 1024)
-            fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
-          else
-            fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
 
-          document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
-          document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize;
-          document.getElementById('fileType').innerHTML = 'Type: ' + file.type;
-        }
-      }
+// Google chrome
+if (!window.BlobBuilder && window.WebKitBlobBuilder)
+	window.BlobBuilder = window.WebKitBlobBuilder;
 
-      function uploadFile() {
-        var fd = new FormData();
-        fd.append("file", document.getElementById('file').files[0]);
-        // TODO: should get it from current form, now gets first csrf token in page
-        fd.append("csrfmiddlewaretoken", document.getElementsByName('csrfmiddlewaretoken')[0].value);
-        var xhr = new XMLHttpRequest();
-        xhr.upload.addEventListener("progress", uploadProgress, false);
-        xhr.addEventListener("load", uploadComplete, false);
-        xhr.addEventListener("error", uploadFailed, false);
-        xhr.addEventListener("abort", uploadCanceled, false);
-        xhr.open("POST", "/bigfiles/uploadjs/");
-        xhr.send(fd);
-      }
+// Firefox
+if (!window.BlobBuilder && window.MozBlobBuilder)
+	window.BlobBuilder = window.MozBlobBuilder;
 
-      function uploadProgress(evt) {
-        if (evt.lengthComputable) {
-          var percentComplete = Math.round(evt.loaded * 100 / evt.total);
-          document.getElementById('progressNumber').innerHTML = percentComplete.toString() + '%';
-        }
-        else {
-          document.getElementById('progressNumber').innerHTML = 'unable to compute';
-        }
-      }
 
-      function uploadComplete(evt) {
-        /* This event is raised when the server send back a response */
-        alert(evt.target.responseText);
-      }
+if (!window.BlobBuilder )
+	alert("no blobbuilder. Use chrome 8+ or firefox 6+")
 
-      function uploadFailed(evt) {
-        alert("There was an error attempting to upload the file.");
-      }
 
-      function uploadCanceled(evt) {
-        alert("The upload has been canceled by the user or the browser dropped the connection.");
-      }
+function fileSelected() {
+	var file = document.getElementById('file').files[0];
+	if(file) {
+		var fileSize = 0;
+		if(file.size > 1024 * 1024)
+			fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
+		else
+			fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
+
+		document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
+		document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize;
+		document.getElementById('fileType').innerHTML = 'Type: ' + file.type;
+	}
+}
+
+
+function uploadFile() {
+	
+	//var blocksize = 1024;
+	//file.size	
+ 
+
+	var file = document.getElementById('file').files[0];
+	
+	var blobCrypt = BlobCrypt(file);
+	blobCrypt.onload = cryptComplete;
+	
+	
+	var reader = new FileReader()
+	reader.readAsArrayBuffer(file);
+	reader.onload = function() {}
+  	reader.onerror = function(e) { alert(e) };
+  	}
+
+	var crypted = new BlobBuilder();
+	crypted.append(file);
+
+	cryptComplete(crypted.getBlob());
+}
+
+function cryptComplete(cryptedFile) {
+	var fd = new FormData();
+	var xhr = new XMLHttpRequest();
+		
+ 	// TODO: should get it from current form, now gets first csrf token in page
+	fd.append("csrfmiddlewaretoken", document.getElementsByName('csrfmiddlewaretoken')[0].value);
+	fd.append("file", );
+	xhr.upload.addEventListener("progress", uploadProgress, false);
+	xhr.addEventListener("load", uploadComplete, false);
+	xhr.addEventListener("error", uploadFailed, false);
+	xhr.addEventListener("abort", uploadCanceled, false);
+	xhr.open("POST", "/bigfiles/uploadjs/");
+	xhr.send(fd);
+}
+
+function uploadProgress(evt) {
+	if(evt.lengthComputable) {
+		var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+		document.getElementById('progressNumber').innerHTML = percentComplete.toString() + '%';
+	} else {
+		document.getElementById('progressNumber').innerHTML = 'unable to compute';
+	}
+}
+
+function uploadComplete(evt) {
+	/* This event is raised when the server send back a response */
+	alert(evt.target.responseText);
+}
+
+function uploadFailed(evt) {
+	alert("There was an error attempting to upload the file.");
+}
+
+function uploadCanceled(evt) {
+	alert("The upload has been canceled by the user or the browser dropped the connection.");
+}
